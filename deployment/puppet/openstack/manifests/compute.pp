11a369d64187b7cd0bf06ebecb2f34de9b5311e3
Andrey <agrebennikov@mirantis.com>
2015-01-28 10:57:16 -0800

    Commented quemu-kvm-rhev in compute manifest

--- deployment/puppet/openstack/manifests/compute.pp
+++ deployment/puppet/openstack/manifests/compute.pp
@@ -185,7 +185,7 @@ class openstack::compute (
 
       # From legacy params.pp
       $libvirt_type_kvm             = $::operatingsystem ? {
-                                    redhat =>  'qemu-kvm-rhev',
+#                                    redhat =>  'qemu-kvm-rhev',
                                     default => 'qemu-kvm',
                                   }
       $guestmount_package_name      = 'libguestfs-tools-c'
