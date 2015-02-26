cb135973e3c8f9a400c236358567fc9734116038
Akirilochkin <akirilochkin@mirantis.com>
2015-02-26 16:45:19 +0100

    storage gw for storage nodes, puplic gw for other

--- deployment/puppet/osnailyfacter/examples/site.pp
+++ deployment/puppet/osnailyfacter/examples/site.pp
@@ -74,14 +74,25 @@ if $::fuel_settings['nodes'] {
     $internal_netmask = get_network_role_property('management', 'netmask')
     #
     $public_int = get_network_role_property('ex', 'interface')
+    $storage_int = get_network_role_property('storage', 'interface')
+
     if $public_int {
       $public_address = get_network_role_property('ex', 'ipaddr')
       $public_netmask = get_network_role_property('ex', 'netmask')
 
-      # TODO(Xarses): remove this after completing merge of
-      # multiple-cluster-networks
-      L23network::L3::Ifconfig<| title == $public_int |> {
-        default_gateway => true
+      if $::fuel_settings['role'] in ['ceph-mon', 'ceph-osd', 'primary-ceph-mon'] {
+        L23network::L3::Ifconfig<| title == $storage_int |> {
+          default_gateway => true
+        }
+        L23network::L3::Ifconfig<| title == $public_int |> {
+          default_gateway => undef
+        }
+      } else {
+        # TODO(Xarses): remove this after completing merge of
+        # multiple-cluster-networks
+        L23network::L3::Ifconfig<| title == $public_int |> {
+          default_gateway => true
+        }
       }
     } else {
       # TODO(Xarses): remove this after completing merge of
